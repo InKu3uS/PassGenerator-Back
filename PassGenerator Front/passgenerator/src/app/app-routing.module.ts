30ebd1e138b1d08f33e3a19f927c5f2e97cb1463
Nefta <neftarguez@gmail.com>
2024-08-21 11:32:50 +0100

    Guard y componente FAQ

--- PassGenerator Front/passgenerator/src/app/app-routing.module.ts
+++ PassGenerator Front/passgenerator/src/app/app-routing.module.ts
@@ -7,16 +7,17 @@ import { LoginComponent } from './components/login/login.component';
 import { GeneratePasswordComponent } from './components/generate-password/generate-password.component';
 import { SavePasswordComponent } from './components/save-password/save-password.component';
 import { FaqComponent } from './components/faq/faq.component';
+import { authGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   //{ path: '**', redirectTo: 'home' }, // For any unmatched routes, redirect to home page
   { path: 'home', component: HomeComponent},
-  { path: 'passwords', component: PasswordsComponent},
-  { path: 'users/list', component: UserListComponent},
+  { path: 'passwords', component: PasswordsComponent, canActivate: [authGuard]},
+  { path: 'users/list', component: UserListComponent, canActivate: [authGuard]},
   { path: 'login', component: LoginComponent},
   { path: 'generate', component: GeneratePasswordComponent},
-  { path: 'save', component: SavePasswordComponent },
+  { path: 'save', component: SavePasswordComponent, canActivate: [authGuard] },
   { path: 'faq', component: FaqComponent },
 ];
 
